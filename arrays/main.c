1d61a121095426030f3c371ab75338598ea1ce49
migusmp <mmp2004sp@gmail.com>
2026-01-28 15:52:40 +0100

    arrays

--- arrays/main.c
+++ arrays/main.c
@@ -38,7 +38,7 @@ int arr_remove_at(int *a, size_t *n, size_t k) {
 
 int arr_remove_at_with_reference(int **pa, size_t *n, size_t k) {
     if (!pa || !*pa || !n)
-        return -1; // pa apunta al puntero, *pa es el array
+        return -1;
     if (*n == 0)
         return -1;
     if (k >= *n)
@@ -54,6 +54,17 @@ int arr_remove_at_with_reference(int **pa, size_t *n, size_t k) {
     return 0;
 }
 
+int insert_on_arr(int **pa, size_t *n, size_t i, int newValue) {
+    if (i > *n)
+        return -1;
+
+    memmove(&(*pa)[i + 1], &(*pa)[i], (*n - i) * sizeof **pa);
+    (*pa)[i] = newValue;
+    (*n)++;
+
+    return 0;
+}
+
 int arr_realloc_after_pop(int **arr, size_t *n) {
     if (n == 0)
         return -1;
@@ -62,6 +73,45 @@ int arr_realloc_after_pop(int **arr, size_t *n) {
     tmp = realloc(*arr, *n * sizeof(int));
     *arr = tmp;
     return 0;
+}
+
+int *clone_arr(int **src, size_t *n) {
+    if (!src || n == 0)
+        return NULL;
+
+    int *dst = malloc(*n * sizeof(int));
+    if (!dst)
+        return NULL;
+
+    memcpy(dst, (*src), *n * sizeof(int));
+
+    return dst;
+}
+
+int *concat_int_arrays(const int *a, size_t na, const int *b, size_t nb,
+                       size_t *out_n) {
+    if (!out_n)
+        return NULL;
+    *out_n = 0;
+
+    if ((!a && na) || (!b && nb))
+        return NULL;
+
+    size_t total = na + nb;
+    if (total == 0)
+        return NULL;
+
+    int *res = malloc(total * sizeof(int));
+    if (!res)
+        return NULL;
+
+    if (na)
+        memcpy(res, a, na * sizeof(int));
+    if (nb)
+        memcpy(res + na, b, nb * sizeof(int));
+
+    *out_n = total;
+    return res;
 }
 
 void print_arr(int *arr, size_t size) {
@@ -102,6 +152,21 @@ int main(void) {
     arr_realloc_after_pop(&a, &n);
     print_arr(a, n);
 
+    printf("INSERTANDO EN EL ARRAY...\n");
+    insert_on_arr(&a, &n, 1, 30);
+    print_arr(a, n);
+
+    printf("arr cloned..\n");
+    int *arr_cloned = clone_arr(&a, &n);
+    print_arr(arr_cloned, n);
+
+    printf("arr concat..\n");
+    size_t concated_len;
+    int *concated_arrs = concat_int_arrays(a, n, arr_cloned, n, &concated_len);
+    print_arr(concated_arrs, concated_len);
+
     free(a);
+    free(arr_cloned);
+    free(concated_arrs);
     return 0;
 }
